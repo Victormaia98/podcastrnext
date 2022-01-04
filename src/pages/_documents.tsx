221d5a918c7817eff3e5ad72d01d536b19a6a7c2
Victor Maia Medeiros <victor_maia98@hotmail.com>
2022-01-04 17:39:44 -0400

    init

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -5,14 +5,13 @@ export default class MyDocument extends Document{
         return(
             <Html>
                 <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <meta charSet="UTF-8" />
+
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet"/>
                 </Head>
                 <body>
-                    <Main>
-
-                    </Main>
+                    <Main />
                     <NextScript>
 
                     </NextScript>
